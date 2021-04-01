d1d83d60c51144462df0ffd8ebe40f02a503e3b3
Kilyakus <aleksandr.kilyakov@gmail.com>
2021-04-01 22:27:14 +0500

    initial commit

--- src/ThemeDefaultAsset.php
+++ src/ThemeDefaultAsset.php
@@ -1,31 +1,14 @@
 <?php
-
-/**
- * @copyright Copyright &copy; Kartik Visweswaran, Krajee.com, 2014 - 2018
- * @package yii2-widgets
- * @subpackage yii2-widget-select2
- * @version 2.1.4
- */
-
 namespace kilyakus\select2;
 
 use kilyakus\widgets\AssetBundle;
 
-/**
- * Asset bundle for the default inbuilt theme for [[Select2]] widget.
- *
- * @author Kartik Visweswaran <kartikv2@gmail.com>
- * @since 1.0
- */
 class ThemeDefaultAsset extends ThemeAsset
 {
-    /**
-     * @inheritdoc
-     */
     public function init()
     {
         $this->setSourcePath(__DIR__ . '/assets');
-        $this->setupAssets('css', ['css/select2-default']);
+        $this->setupAssets('css', ['css/select2-default.min']);
         parent::init();
     }
 }
